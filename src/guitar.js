4c1cf6defa634f64cfd04ad6e17dd2a832ebcff1
sidiousvic <victor6rene@gmail.com>
2019-02-12 21:55:07 +0900

    Added wireframe to guitar, played with position

--- src/guitar.js
+++ src/guitar.js
@@ -18,13 +18,13 @@ import './assets/OBJLoader'
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 10000);
     var controls = new THREE.OrbitControls( camera, renderer.domElement );
-    camera.position.set(-10, 30, 40);   
-    controls.target.set( 0, -5, 0 ); 
+    camera.position.set(-3, 70, -1);   
+    controls.target.set( 0, 0, 0 ); 
+    // controls.autoRotate = true;
 
 
     var loader = new THREE.OBJLoader();
-    var material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-    var guitar = new THREE.Mesh(material);
+    var guitar = new THREE.Mesh;
 
     loader.load(
         'guitar.obj',
@@ -32,23 +32,43 @@ import './assets/OBJLoader'
         object => {
             scene.add(object);
             guitar = object;
+            object.scale.set(1, 1, 1);
+            object.position.set(1, -1, -6);
+            object.traverse( function (child) {
+                if ( child instanceof THREE.Mesh ) {
+                    child.material.wireframe = true;
+                    child.material.needsUpdate = true;
+                }
+            });
         }
         
     );
-    
-    var directionalLight = new THREE.DirectionalLight(0xff0000, 0.5);
-    scene.add( directionalLight );
 
+    var boundingBox = new THREE.Box3();
+
+    boundingBox.setFromObject( guitar );
+    var center = boundingBox.getCenter();
+
+    // set camera to rotate around center of object
+    controls.target = center;
+    
+    var axesHelper = new THREE.AxesHelper( 100 );
+    scene.add( axesHelper );
+
+    
+    var directionalLight = new THREE.DirectionalLight(0xff0000  , 0.9);
+    scene.add( directionalLight );
 
     
     function render(a) {
         requestAnimationFrame(render);
         controls.update();
         renderer.render(scene, camera );
-        guitar.rotation.x += 0.001;
-        guitar.rotation.y -= 0.001;
-
+        // guitar.rotation.x += 0.01;
+        // guitar.rotation.y -= 0.001;
+        // guitar.rotation.z -= 0.1;
         // renderer.render();
+        console.log(camera.position);
     }
     
     function onResize() {
